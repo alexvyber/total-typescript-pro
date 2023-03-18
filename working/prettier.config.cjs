7194e3282db0ee1fad760736856545e211f29678
Alexey Sokolov <alexvyber@gmail.com>
2023-03-18 17:17:18 +0300

    init

---
+++ working/prettier.config.cjs
@@ -0,0 +1,11 @@
+
+module.exports = {
+  printWidth: 100,
+  semi: false,
+  arrowParens: "avoid",
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: "all",
+  singleQuote: false,
+  bracketSameLine: true
+}
